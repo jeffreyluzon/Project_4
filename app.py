8481e93f7368a87d4ca8c2d7c00c54fafede25a5
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-03-17 17:34:27 -0400

    got stiff to render on page

--- app.py
+++ app.py
@@ -1,36 +1,29 @@
+import apikey
 from flask import Flask, render_template
-
-
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-import json
+import requests
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
+params = {
   'start':'1',
-  'limit':'5000',
+  'limit':'5',
   'convert':'USD'
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+  'X-CMC_PRO_API_KEY': apikey.key,
 }
 
-session = Session()
-session.headers.update(headers)
+json = requests.get(url, params=params, headers=headers).json()
 
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+print(json['data'][0]['name'])
+
+bitcoin_as_string = json['data'][0]['name']
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', bitcoin = bitcoin_as_string )
 
 if __name__ == '__main__':
     app.run(debug=True)
